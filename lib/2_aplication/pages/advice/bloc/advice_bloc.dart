b6ed21b3e70b4443a7ab3dc8c5b9525710722584
Guillermo Moral <guillermo@summersolt.com>
2023-01-21 21:34:08 -0300

    Add equatable

--- lib/2_aplication/pages/advice/bloc/advice_bloc.dart
+++ lib/2_aplication/pages/advice/bloc/advice_bloc.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
--- lib/2_aplication/pages/advice/bloc/advice_state.dart
+++ lib/2_aplication/pages/advice/bloc/advice_state.dart
@@ -1,7 +1,10 @@
 part of 'advice_bloc.dart';
 
 @immutable
-abstract class AdvicerState {}
+abstract class AdvicerState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 class AdviceInitial extends AdvicerState {}
 
@@ -10,9 +13,15 @@ class AdviceStateLoading extends AdvicerState {}
 class AdviceStateLoaded extends AdvicerState {
   final String advice;
   AdviceStateLoaded({required this.advice});
+
+  @override
+  List<Object?> get props => [advice];
 }
 
 class AdviceStateError extends AdvicerState {
    final String message;
   AdviceStateError({required this.message});
+
+  @override
+  List<Object?> get props => [message];
 }
